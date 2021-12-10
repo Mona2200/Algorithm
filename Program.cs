e2932702dd65fad502f6896ef95788b3df9cfbd8
Алёна Мелехова <alenamelehova13@gmail.com>
2021-12-10 21:46:04 +0300

    The program is ready.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="Alyona">
+// Copyright (c) 2021 All Rights Reserved
+// </copyright>
 
 namespace Algorithm
 {
-   class Program
+   public class Program
    {
-      static void Main(string[] args)
+      public static void Main()
       {
-         Console.WriteLine("Hello World!");
+         var user = new User();
+         user.Authorization();
       }
    }
 }
---
+++ User.cs
@@ -0,0 +1,73 @@
+﻿// <copyright file="User.cs" company="Alyona">
+// Copyright (c) 2021 All Rights Reserved
+// </copyright>
+
+namespace Algorithm
+{
+   using System;
+   using System.Threading;
+
+   public class User
+   {
+      public string Login { get; set; }
+
+      public string Name { get; set; }
+
+      public bool IsPremium { get; set; }
+
+      public static void ShowAds()
+      {
+         Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+         Thread.Sleep(1000);
+
+         Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+         Thread.Sleep(2000);
+
+         Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+         Thread.Sleep(3000);
+      }
+
+      /// <summary>
+      /// Set user data from the Console.
+      /// </summary>
+      public void SetUserData()
+      {
+         try
+         {
+            Console.Write("Введите ваш логин: ");
+            this.Login = Console.ReadLine();
+            Console.Write("Введите ваше имя: ");
+            this.Name = Console.ReadLine();
+            Console.Write("Есть ли у вас подписка?(да/нет): ");
+            var answer = Console.ReadLine();
+            if (answer == "да")
+            {
+               this.IsPremium = true;
+            }
+            else if (answer == "нет")
+            {
+               this.IsPremium = false;
+            }
+            else
+            {
+               throw new Exception("Введёное выражение не соответствует формату ввода");
+            }
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+            System.Environment.Exit(1);
+         }
+      }
+
+      public void Authorization()
+      {
+         this.SetUserData();
+         Console.WriteLine($"Добро пожаловать, {this.Name}");
+         if (!this.IsPremium)
+         {
+            ShowAds();
+         }
+      }
+   }
+}
